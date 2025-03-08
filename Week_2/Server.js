78a0cccf7c96e2c3bb8f465884567104da4c989c
Fiza-13 <162727064+Fiza-13@users.noreply.github.com>
2025-03-08 21:35:45 +0530

    Add files via upload

---
+++ Week_2/Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req, res) => {
+    return res.send('Hello Jalish Dada & Hasan Dada....');
+});
+
+app.get("/about", (req, res) => {
+    return res.send (`Hello Dear ${req.query.name}`);
+});
+
+
+app.post("/post", (req, res) => { 
+    const{ name, messege } = req.body;
+    res.send(`Hello ${name}!  ${messege} `);
+});
+
+const port = 8006;
+
+app.listen(port,() => console.log(`Server Started on port ${port}!`));
